140c70256ad55da2e1e2b5fea62e77eb46760c68
Lutz Mende <lutz.mende@inquant.de>
2020-09-10 13:02:33 +0200

    checkout shipping address plugin

--- shop/plusplugins/checkout.py
+++ shop/plusplugins/checkout.py
@@ -12,8 +12,11 @@ from shop.conf import app_settings
 from shop.models.cart import CartModel
 from shop.serializers.cart import CartSerializer
 
+import logging
+logger = logging.getLogger('shop')
 
-class ShippingAddressPluginForm(PlusPluginFormBase):
+
+class CheckoutShippingAddressPluginForm(PlusPluginFormBase):
     CHOICES = [
         ('shipping-address-summary', _("Shipping Address Summary")),
     ]
@@ -32,12 +35,12 @@ class ShippingAddressPluginForm(PlusPluginFormBase):
     extra_style, extra_classes, label, extra_css = get_style_form_fields(STYLE_CHOICES)
 
 
-class ShippingAddressPlugin(StylePluginMixin, PlusPluginBase):
-    name = _('Shipping Address')
+class CheckoutShippingAddressPlugin(StylePluginMixin, PlusPluginBase):
+    name = _('Checkout Shipping Address')
     module = _('Shop')
     cache = False
     allow_children = True
-    form = ShippingAddressPluginForm
+    form = CheckoutShippingAddressPluginForm
 
     def get_render_template(self, context, instance, placeholder):
         render_template = instance.glossary.get('render_template')
@@ -46,28 +49,31 @@ class ShippingAddressPlugin(StylePluginMixin, PlusPluginBase):
         render_type = instance.glossary.get('render_type')
         try:
             return select_template([
-                '{}/address/{}.html'.format(app_settings.APP_LABEL, render_type),
-                'shop/address/{}.html'.format(render_type),
+                '{}/checkout/{}.html'.format(app_settings.APP_LABEL, render_type),
+                'shop/checkout/{}.html'.format(render_type),
             ])
         except TemplateDoesNotExist:
-            return get_template('shop/address/shipping-address-summary.html')
+            return get_template('shop/checkout/shipping-address-summary.html')
 
     def render(self, context, instance, placeholder):
         try:
             cart = CartModel.objects.get_from_request(context['request'])
             context['is_cart_filled'] = cart.items.exists()
-            render_type = instance.glossary['render_type']
-            if render_type == 'static':
-                # update context for static cart with items to be endered as HTML
-                cart_serializer = CartSerializer(cart, context=context, label='cart', with_items=True)
-                context['cart'] = cart_serializer.data
-            elif render_type == 'summary':
-                # update context for cart summary to be endered as HTML
-                cart_serializer = CartSerializer(cart, context=context, label='cart')
-                context['cart'] = cart_serializer.data
+
+            if not cart.shipping_address:
+                # try to set shipping address if exist
+                cart.shipping_address = context['request'].customer.shippingaddress_set.first()
+                cart.save()
+
+            context['address'] = cart.shipping_address
+
+            # update context for static cart with items to be endered as HTML
+            cart_serializer = CartSerializer(cart, context=context, label='cart', with_items=True)
+            context['cart'] = cart_serializer.data
+
         except (KeyError, CartModel.DoesNotExist):
             pass
         return super().render(context, instance, placeholder)
 
 
-plugin_pool.register_plugin(ShippingAddressPlugin)
+plugin_pool.register_plugin(CheckoutShippingAddressPlugin)
