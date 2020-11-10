8cb1f38978c6f7101ef009fa57bb8f1a8644a18f
Lutz Mende <lutz.mende@inquant.de>
2020-11-10 19:58:07 +0100

    purchase button plugin now locks button with spinner during order processing

--- shop/static/shop/js/checkout.js
+++ shop/static/shop/js/checkout.js
@@ -6,8 +6,16 @@ var module = angular.module('django.shop.checkout', []);
 module.controller('checkoutPurchaseCtrl', ['$http', '$q', '$scope', function($http, $q, $scope) {
 	var self = this;
     console.log('checkoutPurchaseCtrl');
+    $scope.lock = false;
+
+	$scope.is_disabled = function(is_empty) {
+        if (is_empty == 'True' || $scope.lock == true)
+            return true;
+        return false;
+    }
 
 	$scope.purchaseNow = function(endpoint) {
+        $scope.lock = true;
 		var deferred = $q.defer();
 		$http.post(endpoint).then(function(response) {
             eval(response.data.expression);
